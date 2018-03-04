7110f38b8400545ff18b2f098c1934cc8fefd9e2
Peng Liu <ahappyforest@gmail.com>
2018-03-04 15:48:38 +0800

    improve publish.sh

--- publish.sh
+++ publish.sh
@@ -1,8 +1,11 @@
+git clone git@github.com:ahappyforest/ahappyforest.github.io.git public
 mv public/.git .git_public_tmp
 rm -rf public
 hugo --theme=even --baseUrl="http://ahappyforest.github.io"
 mv .git_public_tmp public/.git
 cd public
+git init
 git add .
 git commit -m 'publish'
+git remote add origin git@github.com:ahappyforest/ahappyforest.github.io.git
 git push origin master --force
